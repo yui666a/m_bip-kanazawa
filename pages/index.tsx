e474bf1b9c15aa2f628a4a7ceb56b593b5f414ff
yui666a <haiso666@gmail.com>
2022-10-19 17:01:47 +0900

    自動デプロイテスト

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,8 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js!</a><br/>
+          カイドクへようこそ！（自動デプロイテスト）
         </h1>
 
         <p className={styles.description}>
